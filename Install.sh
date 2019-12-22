d399bc3c76e74b60899264e5bb47f45fe175f0b0
kaiseru <linuxthony@gmail.com>
2019-12-22 14:49:18 +0100

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -26,6 +26,7 @@ clear
 echo -n "Downloading GameGearZero files..."
 
 	sleep 5
+	echo
 	echo "Adafruit-retrogame.."
 	sleep 2
 	curl -f -s -o /tmp/retrogame https://github.com/kaiseru/GameGearZero/raw/master/retrogame
@@ -37,7 +38,7 @@ echo -n "Downloading GameGearZero files..."
 	echo "theme-gbz35 Rxbrad .."
 	## curl -f -s -o /tmp/GBZ35 https://github.com/rxbrad/es-theme-gbz35.git
     sleep 2
-	echo " Downloading Configuration files.."
+	echo "Downloading Configuration files.."
 	curl -f -s -o /tmp/config.txt https://raw.githubusercontent.com/kaiseru/GameGearZero/master/config.txt
 	curl -f -s -o /tmp/asound.conf https://raw.githubusercontent.com/kaiseru/GameGearZero/master/asound.conf
 	curl -f -s -o /tmp/retrogame.cfg https://raw.githubusercontent.com/kaiseru/GameGearZero/master/retrogame.cfg
