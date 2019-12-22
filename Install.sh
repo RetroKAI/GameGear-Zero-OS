bfdf9cf63f23db6186a3a042a8ba3fceaadb32c9
kaiseru <linuxthony@gmail.com>
2019-12-22 14:37:29 +0100

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -38,10 +38,11 @@ echo -n "Downloading GameGearZero files..."
 	## curl -f -s -o /tmp/GBZ35 https://github.com/rxbrad/es-theme-gbz35.git
     sleep 2
 	echo " Downloading Configuration files.."
-	curl -f -s -o /tmp/config.txt https://github.com/kaiseru/GameGearZero/raw/master/config.txt
-	curl -f -s -o /tmp/asound.conf https://github.com/kaiseru/GameGearZero/raw/master/asound.conf
-	curl -f -s -o /tmp/retrogame.cfg https://github.com/kaiseru/GameGearZero/raw/master/retrogame.cfg
-	curl -f -s -o /tmp/GPIO.service https://github.com/kaiseru/GameGearZero/raw/master/GPIO.service
+	curl -f -s -o /tmp/config.txt https://raw.githubusercontent.com/kaiseru/GameGearZero/master/config.txt
+	curl -f -s -o /tmp/asound.conf https://raw.githubusercontent.com/kaiseru/GameGearZero/master/asound.conf
+	curl -f -s -o /tmp/retrogame.cfg https://raw.githubusercontent.com/kaiseru/GameGearZero/master/retrogame.cfg
+	curl -f -s -o /tmp/GPIO.service https://raw.githubusercontent.com/kaiseru/GameGearZero/master/GPIO.service
+	curl -f -s -o /tmp/TFT.service https://raw.githubusercontent.com/kaiseru/GameGearZero/master/TFT.service
 	echo "OK"
 	sleep 4
 	
